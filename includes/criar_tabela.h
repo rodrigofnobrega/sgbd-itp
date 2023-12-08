a9c6e55e12a5491d12539a6282479d1ecfce3c16
Caio Daniel <127650414+Caio2a7@users.noreply.github.com>
2023-12-08 15:21:12 -0300

    mudança no tipo da função salvar()

--- includes/criar_tabela.h
+++ includes/criar_tabela.h
@@ -3,7 +3,7 @@
 #include "utils.h"
 
 void definir_tipo(int enum_type, Tabela *banco_de_dados, int aux);
-void salvar_arquivo(Tabela *banco_de_dados, int aux);
+int salvar_arquivo(Tabela *banco_de_dados, int aux);
 void criar_tabela();
 
 #endif
