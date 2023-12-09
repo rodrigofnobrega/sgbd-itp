d0acf74e5f9b62cfb5317dd394c68916abb12ec3
Rodrigo Nobrega <rofen1@protonmail.com>
2023-12-08 21:59:51 -0300

    Criando funções para simplificar a manipulação de arquivos

---
+++ includes/utils/file_utills.h
@@ -0,0 +1,11 @@
+#ifndef FILE_UTILS_H
+#define FILE_UTILS_H
+
+#include <stdio.h>
+#include "../utils.h"
+
+char *definir_caminho_tabela(const char *nome_tabela);
+
+FILE *abrir_arquivo(const char *nome_tabela, char modo_abertura);
+
+#endif // FILE_UTILS_H
---
+++ src/utils/file_utils.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "../../includes/utils/file_utills.h"
+#include "../../includes/utils.h"
+
+char *definir_caminho_tabela(const char *nome_tabela) {
+    char *caminho_tabela = malloc(strlen(DB_PATH) + strlen(nome_tabela) + 5);
+
+    if (caminho_tabela == NULL) {
+        perror("Erro ao alocar memória para caminho_tabela\n");
+        exit(EXIT_FAILURE);
+    }
+
+    sprintf(caminho_tabela, "%s%s.txt", DB_PATH, nome_tabela);
+
+    return caminho_tabela;
+}
+
+FILE *abrir_arquivo(const char *nome_tabela, char modo_abertura) {
+    FILE *arquivo;
+
+    char *caminho_tabela = definir_caminho_tabela(nome_tabela);
+
+    switch (modo_abertura) {
+        case 'r':
+            arquivo = fopen(caminho_tabela, "r");
+            break;
+        case 'w':
+            arquivo = fopen(caminho_tabela, "w");
+            break;
+        case 'a':
+            arquivo = fopen(caminho_tabela, "a");
+            break;
+        default:
+            fprintf(stderr, "Modo de abertura inválido: %c\n", modo_abertura);
+            arquivo =  NULL;
+    }
+
+    if (arquivo == NULL) {
+        perror("Erro ao abrir o arquivo");
+        free(caminho_tabela);
+        return NULL;
+    }
+
+    free(caminho_tabela);
+
+    return arquivo;
+}
