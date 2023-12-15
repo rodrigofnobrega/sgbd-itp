26135a8992f0458265bca8076024dda024fc80c9
Caio Daniel <127650414+Caio2a7@users.noreply.github.com>
2023-12-15 18:57:22 -0300

    Add arquivo header

---
+++ includes/utils/search_utils.h
@@ -0,0 +1,11 @@
+#ifndef SEARCH_UTILS_H
+#define SEARCH_UTILS_H
+#include "../../includes/utils.h"
+
+void pesquisa_int(Dados valor, FILE *arquivo, char buffer[], int index_coluna, int pesquisa_opt);
+void pesquisa_float(Dados valor, FILE *arquivo, char buffer[], int index_coluna, int pesquisa_opt);
+void pesquisa_double(Dados valor, FILE *arquivo, char buffer[], int index_coluna, int pesquisa_opt);
+void pesquisa_char(Dados valor, FILE *arquivo, char buffer[], int index_coluna, int pesquisa_opt);
+void pesquisa_string(Dados valor, FILE *arquivo, char buffer[], int index_coluna, int pesquisa_opt);
+
+#endif
