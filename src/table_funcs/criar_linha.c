e49e476e4bcc9e3256b6467660632f61e0484742
Caio Daniel <127650414+Caio2a7@users.noreply.github.com>
2023-12-15 20:22:12 -0300

    Adicionado comentários

--- src/table_funcs/criar_linha.c
+++ src/table_funcs/criar_linha.c
@@ -1,10 +1,11 @@
 /*
 =========================================================================================================
 Arquivo: criar_linha.c
-Data: 13 de dezembro de 2023
-Descrição: O objetivo desse código é criar e formatar corretamente o banco de dados requerido pelo usuário.
-           Requisita o nome do banco, tipos das colunas e o nome de cada uma, formatando duas linhas no arquivo para tal.
-           A fucionalidade de adicionar dados no banco é realizado pelo código criar_linhas.c
+Data modificação: 15 de dezembro de 2023
+Descrição: O objetivo desse código é adicionar as linhas, ou tuplas, para o banco de dados. Demonstrando ao 
+usuário como está formatado as colunas do banco de dados e assim requisitando que o usário digite as linhas
+de acordo com essa formtação. Ademais, ele realiza tratamento de erro quanto ao tipo do dado digitado e se 
+está relacionado corretamente com o da coluna.
 =========================================================================================================
 */
 #include <stdio.h>
@@ -15,6 +16,12 @@ Descrição: O objetivo desse código é criar e formatar corretamente o banco d
 #include "../../includes/utils/file_utils.h"
 #define MAX_TAMANHO_LINHA 100
 
+/*--------------------------------------------------------------------------------
+Função: Printa na tela uma linha horizontal de delimitação formatadas para o tamanho dos caracters do banco.
+Paramêtros: 
+    -Array contendo o tamanho das colunas;
+    -Quantidade de colunas;
+*/
 void mostrar_line(int *tam_colunas, int qtd_colunas) {
     printf("+");
     for (int i = 0; i < qtd_colunas; i++) {
@@ -25,7 +32,13 @@ void mostrar_line(int *tam_colunas, int qtd_colunas) {
     }
     printf("\n");
 }
-
+/*--------------------------------------------------------------------------------
+Função: Printa a tabela em si do banco de dados, utiliza a função mostrar linhas para
+formatação.
+    -Array contendo o nome das colunas;
+    -Array contendo os tipos das colunas;
+    -Quantidade de colunas;
+*/
 void formatar_tabelas(char **nomes_colunas, int *tipos_colunas, int qtd_colunas) {
     int tam_colunas[qtd_colunas];
     for (int i = 0; i < qtd_colunas; i++) {
@@ -72,6 +85,13 @@ void formatar_tabelas(char **nomes_colunas, int *tipos_colunas, int qtd_colunas)
     mostrar_line(tam_colunas, qtd_colunas);
 }
 
+/*--------------------------------------------------------------------------------
+Função: Define o tipo do dado a ser coletado pela.
+Paramêtros: 
+    -Linha lida do banco de dados;
+    -Tipo da coluna;
+    -Tipo variante para receber os dados;
+*/
 Dados definindo_dados(char *linha, int col_type, Dados col_dados){
     switch(col_type){
         case INT:
@@ -93,110 +113,136 @@ Dados definindo_dados(char *linha, int col_type, Dados col_dados){
     }
     return col_dados;
 }
-
+/*--------------------------------------------------------------------------------
+Função: Adiciona as linhas ao banco de dados.
+*/
 void criar_linha() {
+    // Declaração de variáveis
     char banco_nome[STRING_MAX_SIZE];
     FILE *arquivo_ler;
     FILE *arquivo_escrever;
 
+    // Solicita o nome do arquivo que receberá as novas linhas
     printf("Digite o nome do arquivo que desejas adicionar as linhas: ");
     fgets(banco_nome, STRING_MAX_SIZE, stdin);
+
+    // Remove o caractere de nova linha, se existir
     int string_length = strlen(banco_nome);
-    if(banco_nome[string_length-1] == '\n'){
-        banco_nome[string_length-1] = '\0';
+    if (banco_nome[string_length - 1] == '\n') {
+        banco_nome[string_length - 1] = '\0';
     }
 
+    // Abre o arquivo para leitura
     arquivo_ler = abrir_arquivo(banco_nome, 'r');
 
+    // Processa a primeira linha para determinar os tipos de dados das colunas
+    // e os nomes das colunas
     int tam_titulo = strlen("Coluna Nome");
-
     char linha[MAX_TAMANHO_LINHA];
+
     if (fgets(linha, MAX_TAMANHO_LINHA, arquivo_ler) == NULL) {
         perror("Erro ao ler a primeira linha");
         fclose(arquivo_ler);
+        return;
     }
 
     int pos = 0;
     int aux = 1;
     int *col_types = (int *)malloc(sizeof(int) * aux);
 
-    while (sscanf(linha + pos, "%d;", &col_types[aux-1]) == 1) {
-        if(sizeof(col_types[aux-1]/4) > tam_titulo){
-            tam_titulo = col_types[aux-1];
+    while (sscanf(linha + pos, "%d;", &col_types[aux - 1]) == 1) {
+        if (sizeof(col_types[aux - 1] / 4) > tam_titulo) {
+            tam_titulo = col_types[aux - 1];
         }
         pos += strcspn(linha + pos, ";") + 1;
         aux++;
         col_types = realloc(col_types, sizeof(int) * aux * 2);
     }
 
+    // Processa a segunda linha para obter os nomes das colunas
     if (fgets(linha, MAX_TAMANHO_LINHA, arquivo_ler) == NULL) {
         perror("Erro ao ler a segunda linha");
+        free(col_types);
         fclose(arquivo_ler);
+        return;
     }
-    
+
     pos = 0;
     aux = 1;
     char **col_names = (char **)malloc(sizeof(char *) * aux);
-    col_names[aux-1] = (char *)malloc(sizeof(char) * STRING_MAX_SIZE);
+    col_names[aux - 1] = (char *)malloc(sizeof(char) * STRING_MAX_SIZE);
 
-    while (sscanf(linha + pos, "%[^;\n];", col_names[aux-1]) == 1) {
-        if(strlen(col_names[aux-1]) > tam_titulo){
-            tam_titulo = strlen(col_names[aux-1]);
+    while (sscanf(linha + pos, "%[^;\n];", col_names[aux - 1]) == 1) {
+        if (strlen(col_names[aux - 1]) > tam_titulo) {
+            tam_titulo = strlen(col_names[aux - 1]);
         }
         pos += strcspn(linha + pos, ";") + 1;
         aux++;
         col_names = realloc(col_names, sizeof(char *) * 2 * aux);
-        col_names[aux-1] = (char *)malloc(sizeof(char) * STRING_MAX_SIZE);
+        col_names[aux - 1] = (char *)malloc(sizeof(char) * STRING_MAX_SIZE);
     }
-    formatar_tabelas(col_names, col_types, aux-1);
-    
-    //Salvando o último id da tabela para fins de comparação com a entrada do usuário
+
+    // Formata as tabelas com os nomes e tipos de colunas
+    formatar_tabelas(col_names, col_types, aux - 1);
+
+    // Salva o último ID da tabela para fins de comparação com a entrada do usuário
     int ultimo_id;
-    if(fgets(linha, BUFFER_MAX_SIZE, arquivo_ler) == NULL){
+    if (fgets(linha, BUFFER_MAX_SIZE, arquivo_ler) == NULL) {
         ultimo_id = 0;
-    }
-    else{
-        while(fscanf(arquivo_ler, "%d", &ultimo_id) == 1){}
+    } else {
+        while (fscanf(arquivo_ler, "%d", &ultimo_id) == 1) {}
     }
 
     fclose(arquivo_ler);
 
+    // Abre o arquivo para escrita (modo 'a' - append)
     arquivo_escrever = abrir_arquivo(banco_nome, 'a');
 
+    // Solicita a quantidade de linhas a serem adicionadas
     int qtd_linhas;
-    printf("\nDigite quantas linhas ira adicionar a cada coluna acima: ");
+    printf("\nDigite quantas linhas irá adicionar a cada coluna acima: ");
     scanf("%d", &qtd_linhas);
     getchar();
-    Dados *col_dados = (Dados *)malloc(sizeof(Dados) * qtd_linhas * aux-1);
-    for(int i = 0; i < qtd_linhas; i++){
-        printf("Digite a %dª linha: ", i+1);
+
+    // Aloca espaço para armazenar os dados das novas linhas
+    Dados *col_dados = (Dados *)malloc(sizeof(Dados) * qtd_linhas * aux - 1);
+
+    // Loop para receber e escrever as novas linhas no arquivo
+    for (int i = 0; i < qtd_linhas; i++) {
+        printf("Digite a %dª linha: ", i + 1);
 
         fgets(linha, MAX_TAMANHO_LINHA, stdin);
 
-        //Verificando se a chave primária digitada é um inteiro
+        // Verifica se a chave primária digitada é um inteiro
         int id;
-        if(sscanf(linha, "%d", &id) == 0){
+        if (sscanf(linha, "%d", &id) == 0) {
             printf("O id precisa obrigatoriamente ser inteiro.\n");
             break;
-        }
-        else if(id <= ultimo_id){
+        } else if (id <= ultimo_id) {
             printf("O id precisa ser maior que o último da lista ID:[%d], ou maior que 0: \n", ultimo_id);
             break;
         }
 
+        // Remove o caractere de nova linha, se existir
         string_length = strlen(linha);
-        if(linha[string_length-1] == '\n'){
-            linha[string_length-1] = '\0';
+        if (linha[string_length - 1] == '\n') {
+            linha[string_length - 1] = '\0';
         }
+
+        // Escreve a nova linha no arquivo
         fprintf(arquivo_escrever, "\n");
         char *token = strtok(linha, " ");
         while (token != NULL) {
             fprintf(arquivo_escrever, "%s;", token);
-            token = strtok(NULL, " "); 
+            token = strtok(NULL, " ");
         }
     }
+
+    // Libera a memória alocada
     free(col_types);
     free(col_names);
     free(col_dados);
+
+    // Fecha o arquivo
     fclose(arquivo_escrever);
 }
--- src/table_funcs/pesquisar_valor.c
+++ src/table_funcs/pesquisar_valor.c
@@ -1,3 +1,12 @@
+/*
+=========================================================================================================
+Arquivo: pesquisar_valor.c
+Data modificação: 15 de dezembro de 2023
+Descrição: O objetivo desse código é pesquisar em algum banco de dados pre-existinte valores digitados pelo
+usuário e realizar lógicas de (maior que, menor que, igual que) para todos os valores da coluna que o usuário
+escolheu para realizar a pesquisa.
+=========================================================================================================
+*/
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
@@ -7,7 +16,9 @@
 #include "../../includes/utils/string_utils.h"
 #include "../../includes/utils/search_utils.h"
 
-
+/*--------------------------------------------------------------------------------
+Função: Escanea a opção escolhida do usuário para pesquisa nos dados da coluna.
+*/
 int pesquisa_options(){
     int pesquisa_opt;
     printf("1. Valores maior que o valor informado\n2. Valores maior ou igual que o valor informado\n3. Valores igual ao valor informado\n4. Valores menor que o valor informado\n5. Valores menor ou igual que o valor informado\n6. Valores próximo ao valor informado (se aplica apenas se a coluna for do tipo string)\n");
@@ -17,75 +28,102 @@ int pesquisa_options(){
     return pesquisa_opt;
 }
 
-int pesquisar_valor(){
+/*--------------------------------------------------------------------------------
+Função: Principal que realiza todas as operações de leitura, pesquisa e análise, além de importar do search_utils funções para 
+tratar dos tipos dinâmicos dos dados.
+*/
+int pesquisar_valor() {
     FILE *arquivo;
     char banco_nome[STRING_MAX_SIZE];
 
+    // Solicita o nome do arquivo
     printf("Digite o nome do arquivo: ");
     fgets(banco_nome, 100, stdin);
 
-    int string_lenght = strlen(banco_nome);
-    if(banco_nome[string_lenght-1] == '\n'){
-        banco_nome[string_lenght-1] = '\0';
+    // Remove o caractere de nova linha, se existir
+    int string_length = strlen(banco_nome);
+    if (banco_nome[string_length - 1] == '\n') {
+        banco_nome[string_length - 1] = '\0';
     }
 
+    // Abre o arquivo para leitura
     arquivo = abrir_arquivo(banco_nome, 'r');
 
-    if(arquivo == NULL){
+    // Verifica se o arquivo foi aberto corretamente
+    if (arquivo == NULL) {
         return -1;
     }
 
+    // Variáveis para armazenar informações da coluna
     int tipo_coluna;
     int index_coluna;
     char nome_coluna[STRING_MAX_SIZE];
 
+    // Solicita o nome da coluna
     printf("Digite o nome da coluna: ");
     scanf("%s", nome_coluna);
     upper_string(nome_coluna);
     getchar();
 
     char buffer[BUFFER_MAX_SIZE];
-    fgets(buffer, BUFFER_MAX_SIZE, arquivo); 
+    fgets(buffer, BUFFER_MAX_SIZE, arquivo);
 
     int aux = 1;
     int pos = 0;
     int *coluna_tipos = (int *)malloc(sizeof(int) * aux);
-    while(sscanf(buffer + pos, "%d;", &coluna_tipos[aux-1]) == 1){
+
+    // Processa a primeira linha para determinar os tipos de dados das colunas
+    while (sscanf(buffer + pos, "%d;", &coluna_tipos[aux - 1]) == 1) {
         pos += strcspn(buffer + pos, ";") + 1;
         aux++;
         coluna_tipos = realloc(coluna_tipos, sizeof(int) * 2 * aux);
     }
-    
+
     fgets(buffer, BUFFER_MAX_SIZE, arquivo);
 
     int coluna_erro = 0;
     pos = 0;
     aux = 1;
     char **coluna_nomes = (char **)malloc(sizeof(char *) * aux);
-    coluna_nomes[aux-1] = (char *)malloc(sizeof(char) * STRING_MAX_SIZE);
-    while (sscanf(buffer + pos, "%[^;\n];", coluna_nomes[aux-1]) == 1) {
-        if(strcmp(nome_coluna, coluna_nomes[aux-1]) == 0){
-            tipo_coluna = coluna_tipos[aux-1];
-            index_coluna = aux-1;
+    coluna_nomes[aux - 1] = (char *)malloc(sizeof(char) * STRING_MAX_SIZE);
+
+    // Processa a segunda linha para obter os nomes das colunas
+    while (sscanf(buffer + pos, "%[^;\n];", coluna_nomes[aux - 1]) == 1) {
+        if (strcmp(nome_coluna, coluna_nomes[aux - 1]) == 0) {
+            tipo_coluna = coluna_tipos[aux - 1];
+            index_coluna = aux - 1;
             coluna_erro = 1;
         }
         pos += strcspn(buffer + pos, ";") + 1;
         aux++;
         coluna_nomes = realloc(coluna_nomes, sizeof(char *) * 2 * aux);
-        coluna_nomes[aux-1] = (char *)malloc(sizeof(char) * STRING_MAX_SIZE);
-    }   
+        coluna_nomes[aux - 1] = (char *)malloc(sizeof(char) * STRING_MAX_SIZE);
+    }
 
-    if(coluna_erro == 0){ 
+    // Verifica se o nome da coluna é válido
+    if (coluna_erro == 0) {
         printf("Nome da coluna inválida. ");
-        return -1; 
-    };
+        free(coluna_tipos);
+        fclose(arquivo);
+        free(arquivo);
+        for (int i = 0; i < sizeof(coluna_nomes); i++) {
+            free(coluna_nomes[i]);
+        }
+        free(coluna_nomes);
+        return -1;
+    }
 
+    // Variável para armazenar o valor a ser pesquisado
+    Dados valor;
+
+    // Solicita o valor que deseja pesquisar na coluna
     printf("Digite o valor que deseja pesquisar na coluna: ");
     int pesquisa_opt;
-    switch(tipo_coluna){
-        Dados valor;
+
+    // Realiza a pesquisa conforme o tipo da coluna
+    switch (tipo_coluna) {
         case INT:
-            if(scanf("%d", &valor.inteiro) != 1){
+            if (scanf("%d", &valor.inteiro) != 1) {
                 printf("Valor digitado não corresponde ao tipo da coluna. ");
                 break;
             };
@@ -94,7 +132,7 @@ int pesquisar_valor(){
             pesquisa_int(valor, arquivo, buffer, index_coluna, pesquisa_opt);
             break;
         case FLOAT:
-            if(scanf("%f", &valor.flutuante) != 1){
+            if (scanf("%f", &valor.flutuante) != 1) {
                 printf("Valor digitado não corresponde ao tipo da coluna. ");
                 break;
             };
@@ -103,7 +141,7 @@ int pesquisar_valor(){
             pesquisa_float(valor, arquivo, buffer, index_coluna, pesquisa_opt);
             break;
         case DOUBLE:
-            if(scanf("%lf", &valor.real) != 1){
+            if (scanf("%lf", &valor.real) != 1) {
                 printf("Valor digitado não corresponde ao tipo da coluna. ");
                 break;
             };
@@ -112,7 +150,7 @@ int pesquisar_valor(){
             pesquisa_double(valor, arquivo, buffer, index_coluna, pesquisa_opt);
             break;
         case CHAR:
-            if(scanf("%c", &valor.caracter) != 1){
+            if (scanf("%c", &valor.caracter) != 1) {
                 printf("Valor digitado não corresponde ao tipo da coluna. ");
                 break;
             };
@@ -122,7 +160,7 @@ int pesquisar_valor(){
             break;
         case STRING:
             valor.string = (char *)malloc(sizeof(char) * STRING_MAX_SIZE);
-            if(scanf("%s", valor.string) != 1){
+            if (scanf("%s", valor.string) != 1) {
                 printf("Valor digitado não corresponde ao tipo da coluna. ");
                 break;
             };
@@ -134,12 +172,15 @@ int pesquisar_valor(){
             printf("\nErro de tipo. ");
             break;
     }
+
     printf("\n");
+
+    // Fecha o arquivo e libera a memória alocada
     fclose(arquivo);
-    free(arquivo);
-    for(int i = 0; i < sizeof(coluna_nomes); i++){
+    for (int i = 0; i < sizeof(coluna_nomes); i++) {
         free(coluna_nomes[i]);
     }
     free(coluna_nomes);
     free(coluna_tipos);
+    return 0;
 }
